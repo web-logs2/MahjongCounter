d7168e303051c1c5212a3a0d775b153c0a8b21f1
yuhan <yuhan@p1.com>
2020-03-20 23:31:02 +0800

    Adapt dark mode

--- MahjongCounter/ViewController/BaseViewController.swift
+++ MahjongCounter/ViewController/BaseViewController.swift
@@ -12,7 +12,7 @@ class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = UIColor.systemBackground
         navigationItem.backBarButtonItem = UIBarButtonItem.back()
     }
 }
